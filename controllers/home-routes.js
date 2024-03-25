4be34c36845ce30ca6a1960e57b5a4aab4250e69
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-03-25 16:20:19 -0600

    added controller index.js file

---
+++ controllers/home-routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
+
+// GET homepage
+router.get('/', async (req, res) => {
+    try{
+
+    } catch (err) {
+        
+    }
+});
