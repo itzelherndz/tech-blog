34992a1433557fe953b2a7736c4be42074da693e
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-03-25 17:31:09 -0600

    updated home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,26 +1,18 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
 
-// GET homepage
+// GET homepage with 15 most recent posts
 router.get('/', async (req, res) => {
     try{
         const dbPostData = await Post.findAll({
             include : [
-                {
-                    model: Post,
-                    attributes: ['title', 'content'],
-                },
                 {
                     model: User,
                     attributes: ['name']
                 }
             ],
             limit: 15,
-            order: [
-                Post, 
-                'createdAt',
-                'DESC'
-            ],
+            order: [Post, 'createdAt','DESC'],
         });
 
         const posts = dbPostData.map((post) => post.get({plain: true}));
@@ -29,6 +21,36 @@ router.get('/', async (req, res) => {
             posts,
             loggedIn: req.session.loggedIn,
         });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// GET one post
+router.get('/posts/:id', async (req, res) => {
+    try{
+        const dbPostData = await Post.findByPk(req.params.id, {
+            include : [
+                {
+                    model: User,
+                    attributes: ['name']
+                },
+                {
+                    model: Comment,
+                    attribute: ['title','content','createdAt'],
+                }
+            ],
+            order: [
+                Comment, 
+                'createdAt',
+                'DESC'
+            ],
+        });
+
+        const post = dbPostData.get({plain: true});
+        
+        res.render('posts', {post,loggedIn: req.session.loggedIn});
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
