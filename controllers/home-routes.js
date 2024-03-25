86d851afda67d842c11c8462f6467b75e30955e1
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-03-25 17:02:43 -0600

    added homepage route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,8 +4,31 @@ const { User, Post, Comment } = require('../models');
 // GET homepage
 router.get('/', async (req, res) => {
     try{
+        const dbPostData = await Post.findAll({
+            include : [
+                {
+                    model: Post,
+                    attributes: ['title', 'content'],
+                },
+                {
+                    model: User,
+                    attributes: ['name']
+                }
+            ],
+            limit: 15,
+            order: [
+                Post, 'createdAt','DESC'
+            ],
+        });
 
+        const posts = dbPostData.map((post) => post.get({plain: true}));
+
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn,
+        });
     } catch (err) {
-        
+        console.log(err);
+        res.status(500).json(err);
     }
 });
