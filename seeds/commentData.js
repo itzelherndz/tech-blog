65001507dc7e9923aa218e881e18ff33fe11ba2a
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-03-26 08:51:37 -0600

    updated seeds with id's and model associations

--- seeds/commentData.js
+++ seeds/commentData.js
@@ -2,9 +2,16 @@ const {Comment} = require('../models');
 
 const commentData = [
     {
+        id: 1,
         content: 'I agree commenting is a great coding practice! I practice modular coding.',
         user_id: 2,
         post_id: 2
+    },
+    {
+        id:2,
+        content: 'I just started using mySQL, any tips?',
+        user_id: 3,
+        post_id: 1
     }
 ];
 
--- seeds/postData.js
+++ seeds/postData.js
@@ -2,11 +2,13 @@ const {Post} = require('../models');
 
 const postData = [
     {
+        id: 1,
         title: 'mySQL',
         content: '"MySQL, the most popular Open Source SQL database management system, is developed, distributed, and supported by Oracle Corporation." - mySQL Website',
         user_id: 1
     },
     {
+        id: 2,
         title: 'Coding Practices',
         content: 'I am curious, what coding practices do you reccomend? I enjoy leaving comments on my code for later referece and to help out anybody who might work with my code.',
         user_id: 5
