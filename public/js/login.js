107fdbc945f3d34cb277cce4605b5f2b6b349acc
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-03-27 22:42:34 -0600

    added js to public folder

---
+++ public/js/login.js
@@ -0,0 +1,21 @@
+const loginForm = async (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById('username-login').value.trim();
+    const password = document.getElementById('password-login').value.trim();
+    
+    const apiResponse = await fetch('/api/users/login', {
+        method: 'POST',
+        body: {username: username,password: password},
+        headers: { 'Content-Type': 'application/json' },
+      });
+    
+      if (apiResponse.ok) {
+        alert('Success! Logged in.');
+        window.location.assign('/');
+      } else {
+        alert('Login failed. Please try again.');
+      }
+};
+
+document.getElementById('login-btn').addEventListener('click',loginForm);
