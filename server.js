608e31373e6465242547666007e40901aea63437
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-04-16 15:20:05 -0600

    added compare helper

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -27,7 +27,7 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create();
+const hbs = exphbs.create({ helpers });
 // input helpers once created
 
 app.engine('handlebars', hbs.engine);
---
+++ utils/helpers.js
@@ -0,0 +1,9 @@
+module.exports = {
+    compare: (a, b) => {
+        if (a === b){
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
