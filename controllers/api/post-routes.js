419d6cbec414dc7a776f139a87bea18b8356c8ef
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-05-07 22:44:56 -0600

    updated put and delete post routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -25,25 +25,22 @@ router.post('/', async (req,res) => {
 });
 
 // PUT post
-router.put('/', async (req,res) => {
+router.put('/:id', async (req, res) => {
     try {
-        console.log('PUT request body', req.body);
+        const postId = req.params.id; // Get post ID from URL params
 
         const dbPostData = await Post.update(req.body, {
             where: {
-                id: req.session.postId,
+                id: postId, // Use the post ID obtained from the URL params
             }
         });
 
-        if (dbPostData){
+        // Handle response
+        if (dbPostData[0] !== 0) { // Check if any rows were updated
             res.status(200).json(dbPostData);
-          } else {
-            res.status(404).json([
-              {
-                message: 'No post with this id!'
-              }
-            ]);
-          }       
+        } else {
+            res.status(404).json({ message: 'No post with this id!' });
+        }
 
     } catch (err) {
         console.log(err);
@@ -52,30 +49,81 @@ router.put('/', async (req,res) => {
 });
 
 // DELETE post
-router.delete('/', async (req, res) =>{
+router.delete('/:id', async (req, res) => {
     try {
-        console.log('DELETE req session id', req.session.postId);
+        const postId = req.params.id; // Get post ID from URL params
 
         const dbPostData = await Post.destroy({
             where: {
-                id: req.session.postId,
+                id: postId, // Use the post ID obtained from the URL params
             }
         });
 
-        if (dbPostData){
+        // Handle response
+        if (dbPostData !== 0) { // Check if any rows were deleted
             res.status(200).json(dbPostData);
-          } else {
-            res.status(404).json([
-              {
-                message: 'No post with this id!'
-              }
-            ]);
-          }       
+        } else {
+            res.status(404).json({ message: 'No post with this id!' });
+        }
 
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
     }
-})
+});
+
+// // PUT post
+// router.put('/', async (req,res) => {
+//     try {
+//         console.log('PUT request body', req.body);
+
+//         const dbPostData = await Post.update(req.body, {
+//             where: {
+//                 id: req.session.postId,
+//             }
+//         });
+
+//         if (dbPostData){
+//             res.status(200).json(dbPostData);
+//           } else {
+//             res.status(404).json([
+//               {
+//                 message: 'No post with this id!'
+//               }
+//             ]);
+//           }       
+
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).json(err);
+//     }
+// });
+
+// // DELETE post
+// router.delete('/', async (req, res) =>{
+//     try {
+//         console.log('DELETE req session id', req.session.postId);
+
+//         const dbPostData = await Post.destroy({
+//             where: {
+//                 id: req.session.postId,
+//             }
+//         });
+
+//         if (dbPostData){
+//             res.status(200).json(dbPostData);
+//           } else {
+//             res.status(404).json([
+//               {
+//                 message: 'No post with this id!'
+//               }
+//             ]);
+//           }       
+
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).json(err);
+//     }
+// });
 
 module.exports = router;
